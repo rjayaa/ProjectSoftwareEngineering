8339617b805ae466da91e753939abc30d94c32f6
rjayaa <rjaya.lelana03@gmail.com>
2023-05-30 06:41:16 +0700

    Revert "login and sign up fully done"
    This reverts commit 17c59c7eb43e201ebe7b1ad8aa02c44c2fa6649d.

--- tenant_seller/lib/authentication/login.dart
+++ tenant_seller/lib/authentication/login.dart
@@ -1,12 +1,5 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:tenant_seller/mainScreens/home_screen.dart';
 import 'package:tenant_seller/widgets/custom_text_field.dart';
-import 'package:tenant_seller/widgets/error_dialog.dart';
-import 'package:tenant_seller/widgets/loading_dialog.dart';
-
-import '../global/global.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -19,73 +12,6 @@ class _LoginScreenState extends State<LoginScreen> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
-
-  formValidation() {
-    if (emailController.text.isNotEmpty && passwordController.text.isNotEmpty) {
-      // login
-      loginNow();
-    } else {
-      showDialog(
-          context: context,
-          builder: (c) {
-            return ErrorDialog(
-              message: "Please write email/password..",
-            );
-          });
-    }
-  }
-
-  loginNow() async {
-    showDialog(
-        context: context,
-        builder: (c) {
-          return LoadingDialog(
-            message: "Checking Credentials,",
-          );
-        });
-    User? currentUser;
-    await firebaseAuth
-        .signInWithEmailAndPassword(
-      email: emailController.text.trim(),
-      password: passwordController.text.trim(),
-    )
-        .then((auth) {
-      currentUser = auth.user!;
-    }).catchError((error) {
-      Navigator.pop(context);
-      showDialog(
-          context: context,
-          builder: (c) {
-            return ErrorDialog(
-              message: error.message,
-            );
-          });
-    });
-
-    if (currentUser != null) {
-      readDataAndSetDataLocally(currentUser!).then((value) {
-        Navigator.pop(context);
-        Navigator.push(
-            context, MaterialPageRoute(builder: (c) => const HomeScreen()));
-      });
-    }
-  }
-
-  Future readDataAndSetDataLocally(User currentUser) async {
-    await FirebaseFirestore.instance
-        .collection("sellers")
-        .doc(currentUser.uid)
-        .get()
-        .then((snapshot) async {
-      await sharedPreferences!.setString("uid", currentUser.uid);
-      await sharedPreferences!
-          .setString("email", snapshot.data()!["sellerEmail"]);
-      await sharedPreferences!
-          .setString("name", snapshot.data()!["sellerName"]);
-      await sharedPreferences!
-          .setString("photoUrl", snapshot.data()!["sellerAvatarUrl"]);
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -127,7 +53,7 @@ class _LoginScreenState extends State<LoginScreen> {
                 ],
               ),
             ),
-            const SizedBox(
+            SizedBox(
               height: 15,
             ),
             ElevatedButton(
@@ -140,12 +66,9 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
               style: ElevatedButton.styleFrom(
                 primary: Color(0xffFC7115),
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 80, vertical: 20),
+                padding: EdgeInsets.symmetric(horizontal: 80, vertical: 20),
               ),
-              onPressed: () {
-                formValidation();
-              },
+              onPressed: () => print("clicked"),
             ),
           ],
         ),
