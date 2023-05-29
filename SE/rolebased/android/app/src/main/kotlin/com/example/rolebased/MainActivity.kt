c80ebf252dfafc9c7b93729d40f9be459ba00863
rjayaa <rjaya.lelana03@gmail.com>
2023-05-30 06:14:56 +0700

    clear folder and move tenant-seller

--- SE/rolebased/android/app/src/main/kotlin/com/example/rolebased/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.rolebased
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- foodly_resturant_page/android/app/src/main/kotlin/com/example/foodly/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.foodly
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
