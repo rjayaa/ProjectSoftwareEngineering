b53903435c7a1f8d30d317f8bfad132c8bed1da9
rjayaa <rjaya.lelana03@gmail.com>
2023-05-30 07:03:06 +0700

    fix corrupt files

--- tenant_seller/lib/authentication/login.dart
+++ tenant_seller/lib/authentication/login.dart
@@ -1,5 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:tenant_seller/mainScreens/home_screen.dart';
 import 'package:tenant_seller/widgets/custom_text_field.dart';
+import 'package:tenant_seller/widgets/error_dialog.dart';
+import 'package:tenant_seller/widgets/loading_dialog.dart';
+
+import '../global/global.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -12,6 +19,73 @@ class _LoginScreenState extends State<LoginScreen> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+
+  formValidation() {
+    if (emailController.text.isNotEmpty && passwordController.text.isNotEmpty) {
+      // login
+      loginNow();
+    } else {
+      showDialog(
+          context: context,
+          builder: (c) {
+            return ErrorDialog(
+              message: "Please write email/password..",
+            );
+          });
+    }
+  }
+
+  loginNow() async {
+    showDialog(
+        context: context,
+        builder: (c) {
+          return LoadingDialog(
+            message: "Checking Credentials,",
+          );
+        });
+    User? currentUser;
+    await firebaseAuth
+        .signInWithEmailAndPassword(
+      email: emailController.text.trim(),
+      password: passwordController.text.trim(),
+    )
+        .then((auth) {
+      currentUser = auth.user!;
+    }).catchError((error) {
+      Navigator.pop(context);
+      showDialog(
+          context: context,
+          builder: (c) {
+            return ErrorDialog(
+              message: error.message,
+            );
+          });
+    });
+
+    if (currentUser != null) {
+      readDataAndSetDataLocally(currentUser!).then((value) {
+        Navigator.pop(context);
+        Navigator.push(
+            context, MaterialPageRoute(builder: (c) => const HomeScreen()));
+      });
+    }
+  }
+
+  Future readDataAndSetDataLocally(User currentUser) async {
+    await FirebaseFirestore.instance
+        .collection("sellers")
+        .doc(currentUser.uid)
+        .get()
+        .then((snapshot) async {
+      await sharedPreferences!.setString("uid", currentUser.uid);
+      await sharedPreferences!
+          .setString("email", snapshot.data()!["sellerEmail"]);
+      await sharedPreferences!
+          .setString("name", snapshot.data()!["sellerName"]);
+      await sharedPreferences!
+          .setString("photoUrl", snapshot.data()!["sellerAvatarUrl"]);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -53,7 +127,7 @@ class _LoginScreenState extends State<LoginScreen> {
                 ],
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 15,
             ),
             ElevatedButton(
@@ -66,9 +140,12 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
               style: ElevatedButton.styleFrom(
                 primary: Color(0xffFC7115),
-                padding: EdgeInsets.symmetric(horizontal: 80, vertical: 20),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 80, vertical: 20),
               ),
-              onPressed: () => print("clicked"),
+              onPressed: () {
+                formValidation();
+              },
             ),
           ],
         ),
