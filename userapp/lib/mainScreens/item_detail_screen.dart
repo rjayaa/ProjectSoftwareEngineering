3ee62dfd6bdc3e2683bb10e28b74f37895a398bf
rjayaa <rjaya.lelana03@gmail.com>
2023-05-31 11:28:27 +0700

    increment decrement

--- userapp/lib/mainScreens/item_detail_screen.dart
+++ userapp/lib/mainScreens/item_detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:number_inc_dec/number_inc_dec.dart';
 import 'package:userapp/models/Items.dart';
 import 'package:userapp/widgets/app_bar.dart';
 
@@ -12,10 +13,69 @@ class ItemDetailScreen extends StatefulWidget {
 }
 
 class _ItemDetailScreenState extends State<ItemDetailScreen> {
+  TextEditingController counterTextEditingController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: MyAppBar(),
+      body: Column(
+        children: [
+          Image.network(widget.model!.thumbnailUrl.toString()),
+          Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: NumberInputPrefabbed.roundedButtons(
+              controller: counterTextEditingController,
+              incDecBgColor: Color(0xffFC7115),
+              min: 1,
+              max: 5,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              widget.model!.title.toString(),
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              widget.model!.shortInfo.toString(),
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              widget.model!.price.toString(),
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(3.0),
+            child: Center(
+              child: InkWell(
+                onTap: () {
+                  // add to cart
+                },
+                child: Container(
+                    decoration: const BoxDecoration(
+                      color: Color(0xffFC7115),
+                    ),
+                    width: MediaQuery.of(context).size.width - 40,
+                    height: 50,
+                    child: const Center(
+                      child: Text(
+                        "Add to Cart",
+                        style: TextStyle(
+                            color: Colors.white, fontFamily: "Poppins"),
+                      ),
+                    )),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
